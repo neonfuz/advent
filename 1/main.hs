c6b5fec77be4dd62a5265a69fb418113b35628cb
neonfuz <neonfuz@gmail.com>
2017-12-02 02:39:49 -0600

    Made solution generic for both parts

--- 1/main.hs
+++ 1/main.hs
@@ -3,14 +3,16 @@ import Data.Char;
 digits :: String -> [Integer]
 digits string = [ read [c] | c <- string, c `elem` ['0'..'9']]
 
-sumSequential :: [Integer] -> Integer
-sumSequential xs = run (length xs) (length xs `quot` 2)
-  where run samples step = sum $ take samples $ zipWith foo xss (drop step xss)
-        foo a b | a == b = a | otherwise = 0
-        xss = cycle xs
+run :: Int -> Int -> [Integer] -> Integer
+run samples step xs = sum $ [ a | (a,b) <- take samples pairs, a == b ]
+  where pairs = zip tape (drop step tape)
+        tape = cycle xs
+
+part1solution xs = run (length xs) 1 xs
+part2solution xs = run (length xs) (length xs `quot` 2) xs
 
 main :: IO ()
 main = do
   contents <- getContents
-  putStr $ show $ sumSequential $ digits contents
+  putStr $ show $ part2solution $ digits contents
 
