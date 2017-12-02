377e61c798bafd7bc348cc3e16033e848e42e506
neonfuz <neonfuz@gmail.com>
2017-12-02 02:19:26 -0600

    Little refactor

--- 1/main.hs
+++ 1/main.hs
@@ -4,10 +4,10 @@ digits :: String -> [Integer]
 digits string = [ read [c] | c <- string, c `elem` ['0'..'9']]
 
 sumSequential :: [Integer] -> Integer
-sumSequential nums = sum [ fst x | x <- pairs, uncurry (==) x ]
-  where pairs = take numLen (zip infnums (drop (quot numLen 2) infnums))
-        infnums = cycle nums
-        numLen = length nums
+sumSequential xs = run (length xs) (length xs `quot` 2)
+  where run samples step = sum $ take samples $ zipWith foo xss (drop step xss)
+        foo a b | a == b = a | otherwise = 0
+        xss = cycle xs
 
 main :: IO ()
 main = do
