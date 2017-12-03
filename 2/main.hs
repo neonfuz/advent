9bcfeed11b989b5ebceb32400036ae666ea40512
neonfuz <neonfuz@gmail.com>
2017-12-03 01:17:33 -0600

    Day 2 part 2

--- 2/main.hs
+++ 2/main.hs
@@ -6,6 +6,9 @@ main = do
   contents <- getContents
   putStr $ prog contents
 
+checksum :: [Integer] -> Integer
+checksum row = head [ a `quot` b | a <- row, b <- row, a /= b, a `mod` b == 0 ]
+
+prog :: String -> String
 prog input = show $ sum $ map checksum $ table
   where table = map ((map read) . words) $ lines input
-        checksum row = maximum row - minimum row
